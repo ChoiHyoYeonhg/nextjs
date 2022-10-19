ceb27df6dfe755b6050fa46172c8e0b8356966b1
Choi <chy71068@naver.com>
2022-10-19 19:12:11 +0900

    first

--- pages/profile/contect.js
+++ pages/profile/contect.js
@@ -1,12 +1,11 @@
-import React from 'react'
-import styles from '../styles/Home.module.css'
+import React from "react";
 
 export default function contect() {
   return (
-    <div className = {StyleSheet.main}>
-        <h1> 주인장 소개</h1>
-        <p> 안녕하세요. 반갑습니다.</p>
-        <p> 이름 : 최효영 </p>
+    <div className={StyleSheet.main}>
+      <h1> 주인장 소개</h1>
+      <p> 안녕하세요. 반갑습니다.</p>
+      <p> 이름 : 최효영 </p>
     </div>
-  )
+  );
 }
