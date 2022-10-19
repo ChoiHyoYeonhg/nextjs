b8c1b9d5c9cb72c569fc16e0add059886ad1c6c9
Choi <chy71068@naver.com>
2022-10-19 18:52:07 +0900

    first

--- pages/docs/[[...params]].js
+++ pages/docs/[[...params]].js
@@ -1,7 +1,7 @@
 import { useRouter } from "next/router";
 import React from "react";
 
-export default function docs() {
+export default function Docs() {
   const router = useRouter();
   const { params = [] } = router.query;
   console.log(params);
